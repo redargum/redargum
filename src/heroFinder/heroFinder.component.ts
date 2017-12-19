8f04ed904e433a2a63aae6adde05bd14bd6b1853
Alexei Fedotov <alexei.fedotov@gmail.com>
2017-12-19 19:46:08 +0300

    initial commit

---
+++ src/heroFinder/heroFinder.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './heroFinder.component.html',
+  styleUrls: ['./heroFinder.component.css']
+})
+export class AppComponent {
+  title = 'Hero Finder'
+}
