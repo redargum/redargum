05b637bc5b1fb4a7b215c143d925d51d70f2e708
Alexei Fedotov <alexei.fedotov@gmail.com>
2018-01-15 20:00:51 +0300

    added a dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,17 +10,21 @@ import { MessagesComponent } from './messages/messages.component';
 
 import { HeroService } from './hero.service';
 import { MessageService } from './message.service';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
